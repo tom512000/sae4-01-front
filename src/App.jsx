c08fc97ad7a71dc6eef6e8f40cdd925a8e727796
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-02-17 23:52:31 +0100

    Qualité de code avec Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/public/vite.svg'
+import { useState } from "react";
+// eslint-disable-next-line import/no-unresolved
+import viteLogo from "./public/vite.svg";
+import reactLogo from "./assets/react.svg";
+// eslint-disable-next-line import/no-unresolved
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          {/* eslint-disable-next-line react/react-in-jsx-scope */}
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          {/* eslint-disable-next-line react/react-in-jsx-scope */}
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <h1>Vite + React</h1>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <div className="card">
+        {/* eslint-disable-next-line react/button-has-type,no-shadow,react/react-in-jsx-scope */}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <p>
+          {/* eslint-disable-next-line react/react-in-jsx-scope */}
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
