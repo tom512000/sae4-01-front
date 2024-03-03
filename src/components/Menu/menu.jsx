24f4ba414c523b4e08e6fc9445e20b9a8050dce8
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-03-03 18:17:28 +0100

    Ajout du composant Menu

---
+++ src/components/Menu/menu.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./menu.css";
+
+function Menu() {
+  return (
+    <div className="menu">
+      <p>
+        <a href="/">Offres</a>
+      </p>
+      <p>
+        <a href="/">Entreprises</a>
+      </p>
+    </div>
+  );
+}
+
+export default Menu;
