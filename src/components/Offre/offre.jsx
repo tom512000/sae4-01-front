b457fa72b4b77afcb413f063250926b6c7d2c69d
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-03-09 18:17:30 +0100

    Ajout du composant Offre

---
+++ src/components/Offre/offre.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./offre.css";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from "prop-types";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons/faAngleRight";
+
+function Offre({ offre }) {
+  // Manque une requête pour le nom et le logo de l'entreprise à partir de idEntreprise
+
+  return (
+    <div className="offre">
+      <div className="offre_bloc">
+        <div className="offre_logo">
+          <img
+            src={`${offre /* Logo de l'entreprise */}`}
+            alt={`Logo de ${offre /* Nom de l'entreprise */}`}
+          />
+        </div>
+        <div className="offre_informations">
+          <h1>{offre.nom_offre}</h1>
+          <p>nom_ent</p>
+          <div className="offre_donnees">
+            <p>{offre.lieux}</p>
+            <p>{offre.level}</p>
+            <p>{offre.duree}</p>
+            <p>{offre.jour_deb}</p>
+            <p>{offre.nb_place}</p>
+          </div>
+          <div className="offre_bouton">
+            <p>En savoir plus</p>
+            <FontAwesomeIcon icon={faAngleRight} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Offre.propTypes = {
+  offre: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    type_id: PropTypes.number.isRequired,
+    idEntreprise: PropTypes.number.isRequired,
+    nom_offre: PropTypes.string.isRequired,
+    duree: PropTypes.number.isRequired,
+    lieux: PropTypes.string.isRequired,
+    jour_deb: PropTypes.string.isRequired,
+    nb_place: PropTypes.number.isRequired,
+    descrip: PropTypes.string.isRequired,
+    level: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Offre;
