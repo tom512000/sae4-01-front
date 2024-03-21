c675779f62f5aae8df878b760725d515efddfe5c
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-03-21 10:04:42 +0100

    Ajout du composant EditAccount

---
+++ src/components/EditAccount/editAccount.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./editAccount.css";
+// eslint-disable-next-line import/extensions
+import FordableCard from "../FordableCard/fordableCard.jsx";
+// eslint-disable-next-line import/extensions
+import FordableCardInput from "../FordableCardInput/fordableCardInput.jsx";
+// eslint-disable-next-line import/extensions
+import FordableCardButton from "../fordableCardButton/fordableCardButton.jsx";
+
+function EditAccount() {
+  const multiText = (
+    <>
+      Sur cette page, Modifiez facilement vos informations personnel relatives à
+      votre compte comme votre adresse e-mail, votre mot de passe, votre nom,
+      votre prénom, votre numéro de téléphone, votre date de naissance, ainsi
+      que votre CV et votre lettre de motivation au format pdf.
+      <br />
+      <br />
+      Afin de confirmer la modification de votre compte, cliquez sur
+      &quot;Valider&quot;.
+    </>
+  );
+
+  return (
+    <FordableCard
+      pTitle="Prêt à mettre à jour vos données ?"
+      h1Title="REBONJOUR"
+      pText={multiText}
+      h2Title="Modification"
+    >
+      <form action="/">
+        <FordableCardInput type="email" placeholder="Email" />
+        <FordableCardInput type="password" placeholder="Mot de passe" />
+        <FordableCardInput type="text" placeholder="Nom" />
+        <FordableCardInput type="text" placeholder="Prénom" />
+        <FordableCardInput type="tel" placeholder="Numéro de téléphone" />
+        <FordableCardInput type="date" placeholder="Date de naissance" />
+        <FordableCardInput type="file" placeholder="CV (PDF)" />
+        <FordableCardInput
+          type="file"
+          placeholder="Lettre de motivation (PDF)"
+        />
+        <FordableCardButton value="VALIDER" />
+      </form>
+    </FordableCard>
+  );
+}
+
+export default EditAccount;
