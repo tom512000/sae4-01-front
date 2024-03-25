62b1e29fa1d7daac21028719a2c284318284261a
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-03-25 18:50:09 +0100

    Modification du composant Navbar (Ajout des liens manquants et du menu du profil)

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,15 +1,37 @@
-import React from "react";
-import "./navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
-import Header from "../Header";
+import React, { useContext, useEffect, useState } from 'react';
+import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import { useLocation } from 'wouter';
+import { loginUrl, logoutUrl } from '../../services/api/user';
+import { UserContext } from '../../contexts/user/index';
+import ProfileMenu from '../ProfileMenu/profileMenu';
 
 function Navbar() {
+  /* Gestion Header */
+  const userData = useContext(UserContext);
+  const navigate = useLocation()[1];
+  useEffect(() => {
+    if (userData) {
+      navigate('/');
+    }
+  }, [userData, navigate]);
+
+  /* Gestion Menu du profil */
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleProfileMenu = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar_logos">
-        <img src="/public/img/logo-edutech.png" alt="Logo Edutech" />
-        <img src="/public/img/text-edutech.png" alt="Texte Edutech" />
+        <a href="/" className="navbar_image">
+          <img src="/public/img/logo-edutech.png" alt="Logo Edutech" />
+        </a>
+        <a href="/" className="navbar_image">
+          <img src="/public/img/text-edutech.png" alt="Texte Edutech" />
+        </a>
       </div>
       <div className="navbar_buttons">
         <button type="button" className="sign_in">
@@ -17,14 +39,30 @@ function Navbar() {
             <a href="/inscription">Inscription</a>
           </p>
         </button>
-        <Header />
-        <button
-          type="button"
-          className="profile_menu"
-          aria-label="Toggle Profile Menu"
-        >
-          <FontAwesomeIcon icon={faUser} />
-        </button>
+        {userData ? (
+          <>
+            <button type="button" className="log_in_out">
+              <p>
+                <a href={logoutUrl()}>Se déconnecter</a>
+              </p>
+            </button>
+            <button
+              type="button"
+              className="profile_menu"
+              aria-label="Toggle Profile Menu"
+              onClick={toggleProfileMenu}
+            >
+              <FontAwesomeIcon icon={faUser} />
+            </button>
+            {isVisible && <ProfileMenu />}
+          </>
+        ) : (
+          <button type="button" className="log_in_out">
+            <p>
+              <a href={loginUrl()}>Se connecter</a>
+            </p>
+          </button>
+        )}
       </div>
     </div>
   );
