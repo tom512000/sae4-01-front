c01793558e45f130cc38390f07c35c7aaffffe9c
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-03-30 18:02:57 +0100

    Ajout du composant Mentions

---
+++ src/components/Mentions/mentions.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./mentions.css";
+
+function Mentions() {
+  return (
+    <div className="mentions">
+      <h1>Mentions légales</h1>
+      <h2>1. Informations légales</h2>
+      <p>
+        Le site EduTech est édité par Etudiant&Co, une Société à Responsabilité
+        Limitée (SARL) au capital de 1 euro symbolique, immatriculée au Registre
+        du Commerce et des Sociétés de Reims sous le numéro 123 456 789, dont le
+        siège social est situé Chemin des Rouliers, 51687 Reims.
+      </p>
+      <p>
+        Contact : <span>edutech@gmail.com</span>
+      </p>
+      <p>
+        Téléphone : <span>03 26 91 30 02</span>
+      </p>
+      <h2>2. Hébergement</h2>
+      <p>
+        Le site est hébergé juste là, dont le siège social est situé juste ici.
+      </p>
+      <h2>3. Collecte et traitement des données</h2>
+      <p>
+        Conformément à la législation en vigueur, Etudiant&Co collecte et traite
+        les données personnelles des utilisateurs du site dans le respect de sa
+        politique de confidentialité. Pour plus d&apos;informations, veuillez
+        consulter notre politique de confidentialité disponible sur la page
+        Conditions Générales D&apos;Utilisation.
+      </p>
+      <h2>4. Propriété intellectuelle</h2>
+      <p>
+        Le contenu du site, y compris les textes, images, vidéos, et autres
+        éléments, est la propriété exclusive de Etudiant&Co ou de ses
+        partenaires. Toute reproduction ou utilisation sans autorisation
+        préalable est strictement interdite.
+      </p>
+      <h2>5. Conditions d&apos;utilisation</h2>
+      <p>
+        En accédant et en utilisant ce site, l&apos;utilisateur accepte les
+        présentes conditions d&apos;utilisation. Etudiant&Co se réserve le droit
+        de modifier ces conditions à tout moment.
+      </p>
+      <h2>6. Droit applicable et juridiction compétente</h2>
+      <p>
+        Les présentes mentions légales sont régies par le droit applicable en
+        France. En cas de litige, les tribunaux de Reims seront seuls
+        compétents.
+      </p>
+      <h2>7. Inscription</h2>
+      <p>
+        En vous inscrivant sur notre site, vous acceptez de fournir des
+        informations exactes et complètes. Etudiant&Co se réserve le droit de
+        suspendre ou de résilier votre compte en cas de non-respect de ces
+        conditions.
+      </p>
+      <h2>8. Désinscription</h2>
+      <p>
+        Yous pouvez à tout moment demander la désinscription de votre compte en
+        nous contactant à <span>edutech@gmail.com</span>. Vos données seront
+        alors supprimées conformément à notre politique de confidentialité.
+      </p>
+      <h2>9. Offres d&apos;alternance ou de stage</h2>
+      <p>
+        Les offres d&apos;alternance ou de stage publiées sur notre site sont
+        fournies par des tiers. Etudiant&Co ne garantit pas l&apos;exactitude ou
+        la fiabilité de ces offres et ne peut être tenu responsable des
+        conséquences liées à leur utilisation.
+      </p>
+    </div>
+  );
+}
+
+export default Mentions;
