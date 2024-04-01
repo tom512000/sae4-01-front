bdfcc0a5975327ad27a346b6d6f50ab494b6309d
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-04-01 15:17:18 +0200

    Résolution bug js ProfileView (changement du type d'état de user)

--- src/views/ProfileView.jsx
+++ src/views/ProfileView.jsx
@@ -3,7 +3,7 @@ import { getMe } from "../services/api/user";
 import Profile from "../components/Profile/profile";
 
 function ProfileView() {
-  const [meData, setMeData] = useState([]);
+  const [meData, setMeData] = useState();
   useEffect(() => {
     getMe().then((data) => {
       setMeData(data);
