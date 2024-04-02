9c4ef20ea0f42b66e19b0f85ef93b75469f1898c
SIMON Baptiste <baptiste.simon@etudiant.univ-reims.fr>
2024-04-02 14:27:31 +0200

    ajout de la fonctionnalité d'inscription et de désinscription a une offre

--- src/views/OffresListeView.jsx
+++ src/views/OffresListeView.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { fetchAllOffre } from "../services/api/offre";
+import { fetchAllOffre, getInscriptionUserId } from "../services/api/offre";
 import Offres from "../components/Offres/offres";
 import { fetchAllSkill } from "../services/api/skill";
-import Loading from "../components/Loading/loading.jsx";
+import Loading from "../components/Loading/loading";
 
 function OffresListeView() {
   const [offreData, setOffreData] = useState(null);
   const [page, setPage] = useState(1);
   const [skillData, setSkillData] = useState([]);
   const [filtre, setFiltre] = useState([]);
+  const [inscriptionData, setInscription] = useState();
 
   useEffect(() => {
     fetchAllOffre(page, filtre).then((data) => {
@@ -16,6 +17,9 @@ function OffresListeView() {
     });
     fetchAllSkill().then((data) => {
       setSkillData(data);
+    });
+    getInscriptionUserId().then((data) => {
+      setInscription(data);
     });
   }, [page, filtre]);
 
@@ -39,6 +43,7 @@ function OffresListeView() {
         onClick={onClick}
         onSubmit={onSubmit}
         skillData={skillData}
+        inscriptionData={inscriptionData}
       />
     </section>
   );
