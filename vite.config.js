779f647c74f21b24283f8088a7a61355c1ac077c
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2024-04-03 16:17:48 +0200

    Ajout d'une règle sur les fins de ligne

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
